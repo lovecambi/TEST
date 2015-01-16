9097b480c8aa458d89fa9336b1b776ca10efa711
lovecambi <kf96@duke.edu>
2015-01-15 22:10:58 -0500

    Update fig1.R

--- R/fig1.R
+++ R/fig1.R
@@ -2,10 +2,3 @@ library(ggplot2)
 data=data.frame(x=rnorm(100))
 plot=qplot(x, data=data, geom="histogram")
 ggsave(plot,file="../Figs/fig1.pdf")
-############## End R Script ##############
-Create R/fig2.R:
-############## Begin R Script ##############
-library(ggplot2)
-data=data.frame(x=rexp(100))
-plot=qplot(x, data=data, geom="histogram")
-ggsave(plot,file="../Figs/fig2.pdf")
